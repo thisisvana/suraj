642bfe750473c839a29569dc0158084ebf00944b
thisisvana <thisisvana@gmail.com>
2017-05-09 11:02:24 -0700

    tuesday morning work

--- about.php
+++ about.php
@@ -2,27 +2,41 @@
 <?php include('partials/navbar.php'); ?>
 
 <div class="works row expanded">
-  <a href="drawings.php">
-    <div class="panel works-container top-left small-12 medium-6 large-6 column">
-      <div class="front card box">
-        <h1>Drawings</h1>
+
+    <div class="panel about-container small-12 medium-6 large-6 column">
+      <div class="front top-left card box">
+        <h1>Discover <br>Suraj</h1>
       </div>
 
-      <div class="back card box">
+      <div class="back card box about-left-back">
         <h4>Check out recent <br> drawings</h4>
       </div>
     </div>
-  </a>
-  <a href="models.php">
-    <div class="panel works-container top-right small-12 medium-6 large-6 column">
+
+
+    <div class="panel about-container small-12 medium-6 large-6 column">
       <div class="front card box">
-        <h1>3D Models</h1>
+        <h1>About <br>Suraj</h1>
       </div>
-      <div class="back card box">
-        <h4>Check out recent <br> 3D models</h4>
+      <div class="back card box about-right-back">
+        <p>A motivated and passionate FX Artist with versatility to develop high quality organic and
+          inorganic models. Possessing the artistic expertise and technical ability required to work closely
+          with designers to create accurate models. Also having an ability to adapt to a project's mission,
+          vision and values. <br> I am a fast learner and can pick up new concepts and software rapidly.</p>
+        <p>I grew up in a small town, Shimoga in southern India. I have an engineering degree, but the fantastic
+          world of film beckoned me so I left that world behind in pursuit of making effects in movies because
+          the FX spoke to the engineer in me. <br>I am a generalist
+          with understanding of production pipeline and skilled in Simulation Modeling,
+          Texturing and Lighting. My simulation skill set includes rigid body and fluid dynamics.</p>
+
+          <h4>Read / Print / Save or Download my resume</h4>
+          <div class="resume">
+            <a href="img/DT.pdf" target="_blank"><button type="button" class="btn" name="button"><h4>Resume</h4></button></a>
+
+          </div>
       </div>
   </div>
-</a>
+
 </div>
 
   <!-- <div class="banner row expanded">
